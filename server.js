e19e03a55cff6c7907841130cbe67cdbc570d1bb
armyofda12mnkeys <armyofda12mnkeys@gmail.com>
2017-10-18 20:50:44 -0400

    proc server

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(process.env.PORT || 8080);
+app.get('*', function(req, res) {  
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+});
+
+app.listen(port, function(err) {  
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
